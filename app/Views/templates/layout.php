ca5a64254ea0a26bb651110f08ee95e63daeb67d
Gabe <58583246+AinzGabe@users.noreply.github.com>
2025-02-11 12:43:29 +0800

    push

--- app/Views/templates/layout.php
+++ app/Views/templates/layout.php
@@ -68,40 +68,10 @@
      <!-- main content -->
     <main class="main-content"> 
         <?= $this->renderSection('content') ?>
-</main>
+    </main>
 
 
-<!--
-    <div class="container-fluid">
-        
-    <div>
-        <header class="header">
-            <img src="./public/images/icons/logo.png" alt="Juby-An Trucking Services Logo">
-        </header>
-    </div>
-        <div class="sidebar">
-            <div class="profile">
-                <img src="public/images/luffy.jpg" alt="Profile Picture">
-                <h4>Jamaeca Quizon</h4>
-                <p>Admin</p>
-            </div>
-            <nav>
-                <a href="<?= base_url('dashboard') ?>" class="<?= (current_url() == base_url('dashboard')) ? 'active' : '' ?>"><img src="public/images/icons/sidebar/dashboard.png" alt="">Dashboard</a>
-                <a href="<?= base_url('user') ?>" class="<?= (current_url() == base_url('user')) ? 'active' : '' ?>"><img src="public/images/icons/sidebar/user.png" alt="">User Account</a>
-                <a href="<?= base_url('clients') ?>" class="<?= (current_url() == base_url('clients')) ? 'active' : '' ?>"><img src="public/images/icons/sidebar/client.png" alt=""> Client Management</a>
-                <a href="<?= base_url('drivers') ?>" class="<?= (current_url() == base_url('drivers')) ? 'active' : '' ?>"><img src="public/images/icons/sidebar/driver.png" alt=""> Driver and Conductor Management</a>
-                <a href="<?= base_url('bookings') ?>" class="<?= (current_url() == base_url('bookings')) ? 'active' : '' ?>"><img src="public/images/icons/sidebar/booking.png" alt=""> Booking Management</a>
-                <a href="<?= base_url('trucks') ?>" class="<?= (current_url() == base_url('trucks')) ? 'active' : '' ?>"><img src="public/images/icons/sidebar/truck.png" alt=""> Truck Record and Monitoring Maintenance</a>
-                <a href="<?= base_url('maintenance') ?>" class="<?= (current_url() == base_url('maintenance')) ? 'active' : '' ?>"><img src="public/images/icons/sidebar/report.png" alt=""> Report Management</a>
-            </nav>
-            <a href="#" class="logout"><img src="public/images/icons/logout.png" alt="">Logout</a>
-        </div>
 
-        <div class="main-content"> 
- <!--           <?= $this->renderSection('content') ?>
-        </div>
-    </div>
--->
     <script src="<?php echo base_url('/public/assets/js/jquery-3.7.1.js'); ?>"></script> 
     <script src="<?php echo base_url('/public/assets/js/popper.min.js'); ?>"></script> 
     <script src="<?php echo base_url('/public/assets/js/bootstrap.bundle.min.js'); ?>"></script>
