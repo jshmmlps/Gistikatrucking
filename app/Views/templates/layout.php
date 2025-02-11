61b5986c89a259fe9538fd2c4d5d958cbf9c231c
Gabe <58583246+AinzGabe@users.noreply.github.com>
2025-02-11 12:42:46 +0800

    push

--- app/Views/templates/layout.php
+++ app/Views/templates/layout.php
@@ -7,15 +7,78 @@
     <link href="<?= base_url('/public/assets/css/bootstrap.min.css');?>" rel="stylesheet"> 
 
 </head>
-<body>
+<body> 
+    <!-- sidebar -->
+     <div class="sidebar">
+        <nav class="nav flex-column">
+            <span >
+                <img class="rounded mx-auto d-block img-header" src="./public/images/icons/logo.png" alt="Juby-An Trucking Services Logo">
+            </span>
+            <hr class="text-black">
+            <a href="<?= base_url('dashboard') ?>" class="nav-link <?= (current_url() == base_url('dashboard')) ? 'active' : '' ?>">
+                <span class="icon">
+                    <img src="public/images/icons/sidebar/dashboard.png" alt="">
+                </span>
+                <span class="description">Dashboard</span>
+            </a>
+            <a href="<?= base_url('user') ?>" class="nav-link <?= (current_url() == base_url('user')) ? 'active' : '' ?>">
+                <span class="icon">
+                    <img src="public/images/icons/sidebar/user.png" alt="">
+                </span>
+                <span class="description">User Account</span>
+            </a>
+            <a href="<?= base_url('clients') ?>" class="nav-link <?= (current_url() == base_url('user')) ? 'active' : '' ?>">
+                <span class="icon">
+                    <img src="public/images/icons/sidebar/client.png" alt="">
+                </span>
+                <span class="description">Client Management</span>
+            </a>
+            <a href="<?= base_url('drivers') ?>" class="nav-link <?= (current_url() == base_url('user')) ? 'active' : '' ?>">
+                <span class="icon">
+                    <img src="public/images/icons/sidebar/driver.png" alt="">
+                </span>
+                <span class="description">Driver and Conductor Management</span>
+            </a>
+            <a href="<?= base_url('bookings') ?>" class="nav-link <?= (current_url() == base_url('user')) ? 'active' : '' ?>">
+                <span class="icon">
+                    <img src="public/images/icons/sidebar/booking.png" alt="">
+                </span>
+                <span class="description">Booking Management</span>
+            </a>
+            <a href="<?= base_url('trucks') ?>" class="nav-link <?= (current_url() == base_url('user')) ? 'active' : '' ?>">
+                <span class="icon">
+                    <img src="public/images/icons/sidebar/truck.png" alt="">
+                </span>
+                <span class="description">Truck Record and Monitoring Maintenance</span>
+            </a>
+            <a href="<?= base_url('maintenance') ?>" class="nav-link <?= (current_url() == base_url('user')) ? 'active' : '' ?>">
+                <span class="icon">
+                    <img src="public/images/icons/sidebar/report.png" alt="">
+                </span>
+                <span class="description">Report Management</span>
+            </a>
+            <a href="" class="logout">
+                <span class="icon">
+                    <img src="public/images/icons/logout.png" alt="">
+                </span>
+                <span class="description">Log Out</span>
+            </a>
+        </nav>
+     </div>
+     <!-- main content -->
+    <main class="main-content"> 
+        <?= $this->renderSection('content') ?>
+</main>
+
+
+<!--
+    <div class="container-fluid">
+        
     <div>
         <header class="header">
             <img src="./public/images/icons/logo.png" alt="Juby-An Trucking Services Logo">
         </header>
     </div>
-    
-    <div class="container-fluid">
-        <!-- Main Content -->
         <div class="sidebar">
             <div class="profile">
                 <img src="public/images/luffy.jpg" alt="Profile Picture">
@@ -35,9 +98,10 @@
         </div>
 
         <div class="main-content"> 
-            <?= $this->renderSection('content') ?>
+ <!--           <?= $this->renderSection('content') ?>
         </div>
     </div>
+-->
     <script src="<?php echo base_url('/public/assets/js/jquery-3.7.1.js'); ?>"></script> 
     <script src="<?php echo base_url('/public/assets/js/popper.min.js'); ?>"></script> 
     <script src="<?php echo base_url('/public/assets/js/bootstrap.bundle.min.js'); ?>"></script>
