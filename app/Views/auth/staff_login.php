63a1c171fae0c6967ab237600846448650b25da5
Yannah <132041749+havayanas@users.noreply.github.com>
2025-02-21 03:10:31 +0800

    staff

---
+++ app/Views/auth/staff_login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Staff Login</title>
+    <link rel="stylesheet" href="<?= base_url('/public/assets/css/style.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('/public/assets/css/bootstrap.min.css'); ?>">
+    <style>
+        body .container {
+        margin-top: 0 !important;
+    }
+    </style>
+</head>
+<body style="background:linear-gradient(135deg, #003366, #00c6ff);">
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+        <div class="row border rounded-5 p-3 bg-white shadow box-area">
+            <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" >
+                <div class="featured-img">
+                    <img src="<?= base_url('/public/images/icons/logo.png'); ?>" alt="Logo" style="width: 300px;">
+                </div>
+            </div>
+            <div class="col-md-6 right-box">
+                <div class="row align-items-center">
+                    <div class="header-text mb-4">  
+                        <h2>Staff Login</h2>
+                        <?php if(session()->getFlashdata('error')): ?>
+                            <p style="color:red;"><?= session()->getFlashdata('error') ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <form action="<?= base_url('/admin/login/process') ?>" method="post"> <?= csrf_field() ?>
+                        <div class="input-group mb-3">
+                            <input type="text" name="username" required autofocus class="form-control form-control-lg bg-light fs-6"id="emailAddress" placeholder="Email address">
+                        </div>
+                        <div class="input-group mb-1">
+                            <input type="password" name="password" required autofocus class="form-control form-control-lg bg-light fs-6"id="Password" placeholder="Password">
+                        </div>
+                        <div class="input-group mb-5 d-flex justify-content-between">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="formCheck">
+                                <label for="formCheck" class="form-check-label text-secondary"><small>Remember me</small></label>
+                            </div>
+                            <div class="forgot">
+                                <small><a href="#">Forgot password?</a></small>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <button class="btn btn-lg login-btn w-100 fs-6">Login</button>
+                        </div>
+                        <div class="row ">
+                            <small>Don't have an account? <a href="#">Sign Up</a></small>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+     
+    <script src="<?php echo base_url('/public/assets/js/jquery-3.7.1.js'); ?>"></script> 
+    <script src="<?php echo base_url('/public/assets/js/popper.min.js'); ?>"></script> 
+    <script src="<?php echo base_url('/public/assets/js/bootstrap.bundle.min.js'); ?>"></script>
+</body>
+</html>
