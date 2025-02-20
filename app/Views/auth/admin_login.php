0a2e1581edaebeeeeba23791e4b7c3e0a3e52ef3
Yannah <132041749+havayanas@users.noreply.github.com>
2025-02-20 16:39:14 +0800

    login css

--- app/Views/auth/admin_login.php
+++ app/Views/auth/admin_login.php
@@ -2,23 +2,35 @@
 <html>
 <head>
     <title>Admin Login</title>
+    <link rel="stylesheet" href="<?= base_url('/public/assets/css/style.css') ?>">
+    <link href="<?= base_url('/public/assets/css/bootstrap.min.css');?>" rel="stylesheet"> 
+    <link href="https://cdn.datatables.net/2.2.2/css/dataTables.bootstrap5.css" rel="stylesheet"> 
+
 </head>
 <body>
-    <h1>Admin Login</h1>
-    
-    <?php if(session()->getFlashdata('error')): ?>
-        <p style="color:red;"><?= session()->getFlashdata('error') ?></p>
-    <?php endif; ?>
+        <div class="login-container">
+            <h1>Admin Login</h1>
+            <?php if(session()->getFlashdata('error')): ?>
+                <p style="color:red;"><?= session()->getFlashdata('error') ?></p>
+            <?php endif; ?>
 
-    <form action="<?= base_url('/admin/login/process') ?>" method="post">
-        <?= csrf_field() ?>
-        <label>Username:</label>
-        <input type="text" name="username" required><br><br>
+            <form action="<?= base_url('/admin/login/process') ?>" method="post">
+                <?= csrf_field() ?>
+                <div class="input-group">
+                    <label>Username:</label>
+                    <input type="text" name="username" required><br><br>
+                </div>
+                <div class="input-group">
+                    <label>Password:</label>
+                    <input type="password" name="password" required><br><br>
+                </div>
 
-        <label>Password:</label>
-        <input type="password" name="password" required><br><br>
+                <button class="login-btn"type="submit">Login</button>
+            </form>
+        </div>
 
-        <button type="submit">Login</button>
-    </form>
+    <script src="<?php echo base_url('/public/assets/js/jquery-3.7.1.js'); ?>"></script> 
+    <script src="<?php echo base_url('/public/assets/js/popper.min.js'); ?>"></script> 
+    <script src="<?php echo base_url('/public/assets/js/bootstrap.bundle.min.js'); ?>"></script>
 </body>
 </html>
